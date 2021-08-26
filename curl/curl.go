81fdaf37c24c66eb51a322769b376b0b4e61f8b7
zhangyong <zhangyong@qq.com>
2021-08-26 16:11:04 +0800

    add Curl library function

--- curl/curl.go
+++ curl/curl.go
@@ -1,7 +1,9 @@
 package curl
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -43,4 +45,76 @@ func UploadImage(stream, url, fieldName, mimeType string) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+//
+//  Curl 发送http请求
+//  @Description: 发送http请求
+//  @param method 请求方法
+//  @param url
+//  @param query 请求参数 支持（string,map[string]string）
+//  @param headers 请求头
+//  @return []byte
+//  @return error
+//
+func Curl(method, url string, query interface{}, headers map[string]string) ([]byte, error) {
+	methodList := map[string]interface{}{"post": nil, "get": nil}
+	s := strings.ToLower(method)
+	if _, ok := methodList[s]; !ok {
+		return nil, errors.New("请求方法必须是post/get")
+	}
+
+	client := http.Client{}
+
+	data := ""
+	if query != nil {
+		if params, ok := query.(map[string]string); ok {
+			i := 0
+			for k, v := range params {
+				if i > 0 {
+					data += "&"
+				}
+				data += fmt.Sprintf("%s=%s", k, v)
+				i++
+			}
+		} else {
+			data, ok = query.(string)
+			if !ok {
+				return nil, errors.New("参数类型不合法")
+			}
+		}
+	}
+
+	request, err := http.NewRequest(strings.ToUpper(method), url, strings.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	defer request.Body.Close()
+
+	hasUserAgent := false
+	if headers != nil {
+		for hName, hVal := range headers {
+			if strings.ToLower(hName) == "user-agent" {
+				hasUserAgent = true
+			}
+			request.Header.Add(hName, hVal)
+		}
+	}
+
+	if !hasUserAgent {
+		request.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	res, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
